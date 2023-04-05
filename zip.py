8130f17d58cab14d343d0297f31e0804d3653654
malken21 <Malken_Marumasa@outlook.jp>
2023-04-06 05:30:28 +0900

    update

--- zip.py
+++ zip.py
@@ -1,23 +1,50 @@
-import shutil
 import os
 import logging
-
+import zipfile
 from time import sleep
 
-FILE_NAME = 'TypeThreeRPG-Pack'
+#-----設定部分-----start
 
 FORMAT = '%(asctime)s [%(levelname)s]:%(message)s'
+FILE_NAME = 'TypeThreeRPG-Pack.zip'
+ALLOW_LIST = ["pack.mcmeta", "pack.png", "LICENSE", "README.md"]
+
+#-----設定部分-----end
+
+
 
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 logging.info("実行開始")
 
-logging.info("zipファイルにしています...")
-shutil.make_archive(FILE_NAME, format='zip', root_dir='pack')
+def write(ziph, root, file):
+    ziph.write(os.path.join(root, file))
+    logging.info(root+"/" + file)
 
-logging.info("zipファイルが生成されました")
-logging.info("ファイルの場所:\n" + os.path.abspath('TypeThreeRPG-Pack.zip'))
-logging.info("10秒後にこの画面を閉じます")
 
-sleep(10)
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        dir = root.encode().decode()
+        if (dir.startswith("assets", 2)):
+            for file in files:
+                write(ziph, root, file)
+        elif (dir == "."):
+            for file in files:
+                for allowFile in ALLOW_LIST:
+                    if (file == allowFile):
+                        write(ziph, root, file)
+                        continue
+
+
+if __name__ == '__main__':
+    logging.info("zipファイルにしています...")
+    zipf = zipfile.ZipFile(FILE_NAME, 'w', zipfile.ZIP_DEFLATED)
+    zipdir('.', zipf)
+    zipf.close()
+
+    logging.info("zipファイルが生成されました")
+    logging.info("ファイルの場所:\n" + os.path.abspath(FILE_NAME))
+    logging.info("10秒後にこの画面を閉じます")
+
+    sleep(10)
 
 logging.info("プログラムを終了します")
