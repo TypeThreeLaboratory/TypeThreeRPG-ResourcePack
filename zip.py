36497ed36f9729378a74b3d8adbab26179455817
malken21 <Malken_Marumasa@outlook.jp>
2023-04-14 18:25:56 +0900

    Update zip.py

--- zip.py
+++ zip.py
@@ -2,20 +2,20 @@ import os
 import logging
 import zipfile
 import hashlib
-from time import sleep
 
-#-----設定部分-----start
+# -----設定部分-----start
 
 FORMAT = '%(asctime)s [%(levelname)s]:%(message)s'
 FILE_NAME = 'TypeThreeRPG-Pack.zip'
 ALLOW_LIST = ["pack.mcmeta", "pack.png", "LICENSE", "README.md"]
+DIRECTORY = "assets"
 
-#-----設定部分-----end
-
+# -----設定部分-----end
 
 
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 logging.info("実行開始")
+
 
 def write(ziph, root, file):
     ziph.write(os.path.join(root, file))
@@ -25,7 +25,7 @@ def write(ziph, root, file):
 def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
         dir = root.encode().decode()
-        if (dir.startswith("assets", 2)):
+        if (dir.startswith(DIRECTORY, 2)):
             for file in files:
                 write(ziph, root, file)
         elif (dir == "."):
@@ -46,10 +46,8 @@ logging.info("ファイルの場所:\n" + os.path.abspath(FILE_NAME))
 
 
 with open(FILE_NAME, 'rb') as file:
-    logging.info('sha1:  ' +  hashlib.sha1(file.read()).hexdigest())
+    logging.info('sha1:  ' + hashlib.sha1(file.read()).hexdigest())
 
-logging.info("10秒後にこの画面を閉じます")
-
-sleep(10)
+input("Enterキーで、この画面を閉じます")
 
 logging.info("プログラムを終了します")
